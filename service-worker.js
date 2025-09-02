ea6fea266810fecc03227d0cc2d3d994380ea2f3
claytinho1002-hub <claytinho1002@gmail.com>
2025-09-02 07:33:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('tiicoo-barbeiro').then(cache => {
+    caches.open("barbearia-store").then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
+        "/",
+        "/index.html",
+        "/manifest.json"
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
